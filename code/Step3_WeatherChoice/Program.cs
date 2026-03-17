02f3ccdbf26ccda6bf4316d11639a273a3eb7083
bijwht7 <s9061722818@yandex.ru>
2026-03-17 14:41:09 +0300

    Step 3: conditional algorithm and if/else in C#

---
+++ code/Step3_WeatherChoice/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Выбор одежды по погоде");
+Console.Write("Введите текущую температуру");
+int temperature = Convert.ToInt32(Console.ReadLine());
+if (temperature >= 20)
+{
+    Console.WriteLine("Наденьте легкую одежду");
+
+}
+else
+{
+    Console.WriteLine("Наденьте теплую одежду");
+}
+Console.WriteLine("Хорошего дня!");
